44232d380e8a2c7f6d84fb99488d0b934b4cd702
tothmrtn <102242319+tothmrtn@users.noreply.github.com>
2022-10-19 16:05:30 +0200

    Add files via upload

---
+++ backend/src/main/java/net/market/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package net.market.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.market.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+	
+	
+}
